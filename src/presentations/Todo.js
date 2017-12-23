e757f0ecc3c481a12a8f3a49ef04f73d572c3ffd
Daria Marczak <31421134+daria-marczak@users.noreply.github.com>
2017-12-23 19:28:18 +0100

    Update Todo.js

--- src/presentations/Todo.js
+++ src/presentations/Todo.js
@@ -4,9 +4,8 @@ import style from "./Todo.css";
 const Todo = (props) => {
     return
     <div className={style.Todo}>
-        <li>
-            {props.content.text}</li>
-            <button type="button" onClick={() => props.remove(props.content.id)}>x</button>
+        <li>{props.content.text}</li>
+        <button type="button" onClick={() => props.remove(props.content.id)}>x</button>
     </div>
 };
 
